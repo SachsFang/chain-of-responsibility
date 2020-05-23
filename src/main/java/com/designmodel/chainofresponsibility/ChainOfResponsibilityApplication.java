24e0ceca7f6263bddd790a7d82bfabbd453a6f9c
SachsFang <1139110477@qq.com>
2020-05-23 11:23:31 +0800

    init

---
+++ src/main/java/com/designmodel/chainofresponsibility/ChainOfResponsibilityApplication.java
@@ -0,0 +1,13 @@
+package com.designmodel.chainofresponsibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainOfResponsibilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainOfResponsibilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/designmodel/chainofresponsibility/ChainOfResponsibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designmodel.chainofresponsibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainOfResponsibilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
